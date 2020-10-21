f7d4f231262d3b2f6a7da1e097e7c5464eac6fd8
pedutShaspi <pedutshaspi@carmel6000.amitnet.org>
2020-10-21 17:35:16 +0300

    option add in eneral table work

--- client/src/component/GeneralTable.js
+++ client/src/component/GeneralTable.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import RowData from './RowData';
+import { withRouter } from "react-router-dom";
 
 class GeneralTable extends React.Component {
     constructor(props) {
         super();
         this.state = {  }
     }
+    onClickAdd = () => {
+        this.props.history.push(this.props.location.pathname + 'Add');
+      };
     render() { 
         return ( 
             <div>
@@ -23,10 +27,10 @@ class GeneralTable extends React.Component {
                                                                             categors={this.props.categors}/>})
                     }
                 </div>
-                <div className='addingButton'></div>
+                <div className='addingButton' onClick={this.onClickAdd}></div>
             </div>
          );
     }
 }
  
-export default GeneralTable;
+export default withRouter(GeneralTable);
