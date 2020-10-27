3054f39a23bef87930ac700ade633fe115997b2e
pedutShaspi <pedutshaspi@carmel6000.amitnet.org>
2020-10-27 16:50:23 +0200

    pulling for validetion

--- client/src/component/schoolClassData.js
+++ client/src/component/schoolClassData.js
@@ -2,23 +2,22 @@ import React from 'react';
 import Select from 'react-select';
 import SelectStyle from '../style/selectStyle'
 import '../style/AnimationW3School.css'
-import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 class ClassData extends React.Component {
     constructor(props) {
         super();
-        this.state = { 
+        this.state = {
             showClass: true,
         }
-        this.teachers= [{id: 1, name:'נורית cכהן'},{id: 2, name:'נורsית כהן'},{id: 3, name:'נוריתa כהן'}] // Will need to get the list from QSL
+        this.teachers = [{ id: 1, name: 'נורית cכהן' }, { id: 2, name: 'נורsית כהן' }, { id: 3, name: 'נוריתa כהן' }] // Will need to get the list from QSL
     }
 
     //Return the techer list as list of object for the Select.
     makeTeacherOption = (selectKey) => {
         let options = []
         this.teachers.map((teacherData) => {
-            if (this.props.classData.chosenTeachers.filter((teacher) => {return teacher.id === teacherData.id}).length === 0){
-                options.push({ value: teacherData.name, label: teacherData.name, name: this.props.classIndex,id: teacherData.id, selectKey: selectKey}) 
+            if (this.props.classData.chosenTeachers.filter((teacher) => { return teacher.id === teacherData.id }).length === 0) {
+                options.push({ value: teacherData.name, label: teacherData.name, name: this.props.classIndex, id: teacherData.id, selectKey: selectKey })
             }
         })
         return options
@@ -32,51 +31,56 @@ class ClassData extends React.Component {
             let teacerDefaultValue = this.props.classData.chosenTeachers[i].name
             let selectOptions = this.makeTeacherOption(i);
             teachersSelections.push(<div className='TeachersSelect' key={this.props.classData.chosenTeachers[i].id}>
-                                        {/* <img className="removeTeachersSelectIcon" onClick={()=>this.props.removeTeacherToClass(this.props.classIndex,i)} src="/icons/ionic-ios-close.svg" /> */}
-                                        <Select className='editSchoolClassTeacherSelect' 
-                                                styles={SelectStyle()} 
-                                                options={selectOptions}
-                                                selectKey = {i}
-                                                defaultValue={{ value: teacerDefaultValue, label: teacerDefaultValue }}
-                                                onChange={this.props.chooseTeacher}/>
-                                        <img className="removeTeachersSelectIcon" onClick={()=>this.props.removeTeacherToClass(this.props.classIndex,i)} src="/icons/delete.svg" /></div>)
+                <Select className='editSchoolClassTeacherSelect'
+                    styles={SelectStyle()}
+                    options={selectOptions}
+                    selectKey={i}
+                    defaultValue={{ value: teacerDefaultValue, label: teacerDefaultValue }}
+                    onChange={this.props.chooseTeacher} />
+                <img className="removeTeachersSelectIcon" onClick={() => this.props.removeTeacherFromClass(this.props.classIndex, i)} src="/icons/delete.svg" /></div>)
         }
         return teachersSelections
     }
 
     //Get the prev state and change the showClass to the opposite value (if it's true it's will be false...).
     transitionIcon = () => {
-        this.setState((prevState)=>{return {showClass: !prevState.showClass}})
+        this.setState((prevState) => { return { showClass: !prevState.showClass } })
     }
-   
 
-    render() { 
-        return ( 
+
+    render() {
+        return (
             <>
-                <img src='/icons/ionic-ios-arrow-down.svg' className={this.state.showClass ? 'showClassButton' : 'hideClassButton'} onClick={this.transitionIcon}></img>
-                <input value={this.props.classData.name} onChange={this.props.handleChange} name={'name_'+this.props.classIndex} placeholder='הכנס שם כיתה' className='editSchoolClassesInput inputFields'></input>
+                <p class='error' style={{ display: this.props.classData.classNameError.toShow }}>{this.props.classData.classNameError.mess}</p>
+                
+                <div className='TeachersSelect' >
+                    <img src='/icons/ionic-ios-arrow-down.svg' className={this.state.showClass ? 'showClassButton' : 'hideClassButton'} onClick={this.transitionIcon}></img>
+                    <input value={this.props.classData.name} onChange={this.props.handleChange} placeholder='הכנס שם בית ספר' name={'name_' + this.props.classIndex} placeholder='הכנס שם כיתה' className='editSchoolClassesInput inputFields'></input>
+                    <img className="removeClass" onClick={() => this.props.removeClass(this.props.classIndex)} src="/icons/delete.svg" />
+
+                </div>
                 {/*w3-animate-opacity class make sure that the fading will happens only once. */}
-                <div className={this.state.showClass ? 'w3-animate-fading w3-animate-opacity showSchoolClassTeacher': 'hideSchoolClassTeacher'}>
-                        <label for='schoolClassTeacher' className='editSchoolClassTeacherLable'>מורים:</label>
-                        <div className='allEditSchoolClassTeacherSelect'>
-                            {
-                                this.returnTeahcersSelections()
-                            }
+                <div className={this.state.showClass ? 'w3-animate-fading w3-animate-opacity showSchoolClassTeacher' : 'hideSchoolClassTeacher'}>
+                    <label for='schoolClassTeacher' className='editSchoolClassTeacherLable'>מורים:</label>
+                    <div className='allEditSchoolClassTeacherSelect'>
+                        {
+                            this.returnTeahcersSelections()
+                        }
+                    </div>
+                    <div className='editSchoolClassTeacherButtons'>
+                        <div className='editSchoolClassAddExistTeacher addSomethingNew' onClick={() => { this.props.addTeacherToClass(this.props.classIndex) }}>
+                            <img className='addIcon' src='/icons/addicon.svg'></img>
+                            <p className='addTitle'>הוסף מורה קיים</p>
                         </div>
-                        <div className='editSchoolClassTeacherButtons'>
-                            <div className='editSchoolClassAddExistTeacher addSomethingNew' onClick={()=>{this.props.addTeacherToClass(this.props.classIndex)}}>
-                                <img className='addIcon' src='/icons/addicon.svg'></img>
-                                <p className='addTitle'>הוסף מורה קיים</p>
-                            </div>
-                            <div className='addSomethingNew'>
-                                <img className='addIcon' src='/icons/addicon.svg'></img>
-                                <p className='addTitle'>הוסף מורה חדש</p>
-                            </div>
+                        <div className='addSomethingNew'>
+                            <img className='addIcon' src='/icons/addicon.svg'></img>
+                            <p className='addTitle'>הוסף מורה חדש</p>
                         </div>
+                    </div>
                 </div>
             </>
-         );
+        );
     }
 }
- 
+
 export default ClassData;
--- client/src/pages/AddStudent.jsx
+++ client/src/pages/AddStudent.jsx
@@ -6,24 +6,30 @@ import "../style/AddStudentStyle.css";
 import ArrowNavBar from '../component/ArrowNavBar'
 
 class AddStudent extends React.Component {
-    constructor(props) { 
+    constructor(props) {
         super();
-        this.state = {  studentName: '',
-                        userName: '',
-                        password: '',
-                        school: '',
-                        chosenClasses: [],
-                        allClasses: ["א'2", "ב'2", "ג'2"]}
+        this.state = {
+            studentNameError: { toShow: 'none', mess: '' },
+            studentName: '',
+            userNameError: { toShow: 'none', mess: '' },
+            userName: '',
+            passwordError: { toShow: 'none', mess: '' },
+            password: '',
+            schoolNameError: { toShow: 'none', mess: '' },
+            school: '',
+            chosenClasses: [],
+            allClasses: [{id: 1, name:"א'2"},{id: 2, name:"ב'2"},{id: 3, name:"ג'2"}]
+        }
         this.allSchoole = ['עשה חיל', 'בית ספר עם שם אחר']
     }
 
     //Return the classes list as list of object for the Select.
     makeClassesOption = (indexSelect) => {
         let options = []
-        this.state.allClasses.map((nameClass) => {
-            if (this.state.chosenClasses.filter((chosenClassName) => {return chosenClassName === nameClass}).length === 0){
-                options.push({ value: nameClass, label: nameClass, classIndex: indexSelect}) 
-            }   
+        this.state.allClasses.map((classData) => {
+            if (this.state.chosenClasses.filter((chosenClassData) => { return chosenClassData.name === classData.name }).length === 0) {
+                options.push({ value: classData.name, label: classData.name, classIndex: indexSelect, id: classData.id })
+            }
         })
         return options
     }
@@ -32,9 +38,9 @@ class AddStudent extends React.Component {
     makeSchoolOption = (indexSelect) => {
         let options = []
         this.allSchoole.map((nameSchool) => {
-            if (nameSchool !== this.state.school){
-                options.push({ value: nameSchool, label: nameSchool, SchoolIndex: indexSelect}) 
-            }   
+            if (nameSchool !== this.state.school) {
+                options.push({ value: nameSchool, label: nameSchool, SchoolIndex: indexSelect })
+            }
         })
         return options
     }
@@ -42,25 +48,30 @@ class AddStudent extends React.Component {
     returnClassesSelections = () => {
         let classesSelections = [];
         for (let i = 0; i < this.state.chosenClasses.length; i++) {
-            classesSelections.push(<Select className='studentClass'
-                                            styles={SelectStyle()} 
-                                            options={this.makeClassesOption(i)} 
-                                            onChange={this.chooseClass}
-                                            defaultValue={{ value: this.state.chosenClasses[i], label: this.state.chosenClasses[i] }}/>)
+            classesSelections.push(
+                <div className='TeachersSelect' key={this.state.chosenClasses[i].id}>
+                    <Select className='studentClass'
+                        styles={SelectStyle()}
+                        options={this.makeClassesOption(i)}
+                        onChange={this.chooseClass}
+                        defaultValue={{ value: this.state.chosenClasses[i].name, label: this.state.chosenClasses[i].name }} />
+                    <img className="removeClass" onClick={() => this.removeClass(i)} src="/icons/delete.svg" />
+                </div>)
         }
         return classesSelections
     }
 
     addClassOption = () => {
-        this.setState((prevState)=>{
-            prevState.chosenClasses.push('שייך לכיתה')
-            return {chosenClasses: prevState.chosenClasses}})
+        this.setState((prevState) => {
+            prevState.chosenClasses.push({id: -1*prevState.chosenClasses.length, name: 'שייך לכיתה'})
+            return { chosenClasses: prevState.chosenClasses }
+        })
     }
 
     chooseClass = (e) => {
-        this.setState((prevState)=>{
-            prevState.chosenClasses[e.classIndex] = e.value;
-            return {chosenClasses: prevState.chosenClasses}
+        this.setState((prevState) => {
+            prevState.chosenClasses[e.classIndex] = {id: e.id, name: e.value};
+            return { chosenClasses: prevState.chosenClasses }
         })
     }
 
@@ -68,49 +79,166 @@ class AddStudent extends React.Component {
         let updateData = e.target
         this.setState((prevState) => {
             prevState[updateData.name] = updateData.value
-            return {[updateData.name]: prevState[updateData.name]}
+            return { [updateData.name]: prevState[updateData.name] }
         })
     }
 
     chooseSchool = (e) => {
-        this.setState({school: e.value})
+        this.setState({ school: e.value })
     }
 
-    render() { 
-        return ( 
+    removeClass = (classIndex) => {
+        this.setState((prevState) => {
+            let tempData = [...prevState.chosenClasses]
+            tempData.splice(classIndex, 1);
+            return { chosenClasses: tempData }
+        })
+    }
+
+    saveButton = (e) => {
+        e.preventDefault();
+        let allOk = true
+        /* data validetion  */
+        // ----------student name validetion-------------------
+        if (this.state.studentName.length === 0) {
+            this.setState((prevState) => {
+                prevState.studentNameError.toShow = 'block'
+                prevState.studentNameError.mess = '** חייב להכניס שם תלמיד **'
+                return { studentNameError: prevState.studentNameError }
+            })
+            allOk = false
+        } else if ((/[a-z]/).test(this.state.studentName) || (/[A-Z]/).test(this.state.studentName) || (/[!@#$"%^,.&*()_+\=\[\]{};:\\|<>\/?~`]/).test(this.state.studentName)) {
+            this.setState((prevState) => {
+                prevState.studentNameError.toShow = 'block'
+                prevState.studentNameError.mess = '** שם תלמיד לא תקין **'
+                return { studentNameError: prevState.studentNameError }
+            })
+            allOk = false
+        } else if (this.state.studentName.includes("'") || this.state.studentName.includes('-')) {
+            if (!((/[\u0590-\u05FF]+[-]+[\u0590-\u05FF]/).test(this.state.studentName) || (/[\u0590-\u05FF]+[']/).test(this.state.studentName))) {
+                this.setState((prevState) => {
+                    prevState.studentNameError.toShow = 'block'
+                    prevState.studentNameError.mess = '** שם תלמיד לא תקין **'
+                    return { studentNameError: prevState.studentNameError }
+                })
+                allOk = false
+            }
+        } else {
+            this.setState({ studentNameError: { toShow: 'none', mess: '' } })
+        }
+        
+        // ----------user name validetion-------------------
+        if (this.state.userName.length === 0){
+            this.setState((prevState) => {
+                prevState.userNameError.toShow = 'block'
+                prevState.userNameError.mess = '** חייב להכניס שם משתמש **'
+                return { userNameError: prevState.userNameError }
+            })
+            allOk = false
+        } else if (this.state.userName.length > 30 || this.state.userName.length < 8){
+            this.setState((prevState) => {
+                prevState.userNameError.toShow = 'block'
+                prevState.userNameError.mess = '** שם משתמש לא תקין **'
+                return { userNameError: prevState.userNameError }
+            })
+            allOk = false
+        } else if (!(/[!@#$"%^,.&*()_+\=\[\]{}'-;:\\|<>\/?~`\s]/).test(this.state.userName)){
+            this.setState((prevState) => {
+                prevState.userNameError.toShow = 'block'
+                prevState.userNameError.mess = '** שם משתמש לא תקין **'
+                return { userNameError: prevState.userNameError }
+            })
+            allOk = false
+        } else {
+            this.setState({ userNameError: { toShow: 'none', mess: '' } })
+        }
+
+        // ---------------password validetion-------------------
+        if (this.state.password.length === 0){
+            this.setState((prevState) => {
+                prevState.passwordError.toShow = 'block'
+                prevState.passwordError.mess = '** חייב להכניס סיסמא **'
+                return { passwordError: prevState.passwordError }
+            })
+            allOk = false
+        } else if (this.state.password.length < 30 || this.state.password.length < 8){
+            this.setState((prevState) => {
+                prevState.passwordError.toShow = 'block'
+                prevState.passwordError.mess = '** סיסמא לא תקינה **'
+                return { passwordError: prevState.passwordError }
+            })
+            allOk = false
+        } else if (!(/[!@#$"%^,.&*()_+\=\[\]{}'-;:\\|<>\/?~`\s]/).test(this.state.password)){
+            this.setState((prevState) => {
+                prevState.passwordError.toShow = 'block'
+                prevState.passwordError.mess = '** סיסמא לא תקינה **'
+                return { passwordError: prevState.passwordError }
+            })
+            allOk = false
+        } else {
+            this.setState({ passwordError: { toShow: 'none', mess: '' } })
+        }
+
+        // ---------------school name validetion-------------------
+        if (this.state.school.length === 0){
+            this.setState((prevState) => {
+                prevState.schoolNameError.toShow = 'block'
+                prevState.schoolNameError.mess = '** חייב לבחור שם בית ספר **'
+                return { schoolNameError: prevState.schoolNameError }
+            })
+            allOk = false
+        } else {
+            this.setState({ schoolNameError: { toShow: 'none', mess: '' } })
+        }
+
+
+        //after all the validetion we need to send the data to sql
+        if (allOk) {
+            this.props.history.goBack() // after saving go back
+        }
+    }
+
+    render() {
+        return (
             <div>
                 <ArrowNavBar />
                 <form className='formData addStudentForm'>
                     <label for='studentName'>שם התלמיד:</label>
+                    <p class='error' style={{ display: this.state.studentNameError.toShow }}>{this.state.studentNameError.mess}</p>
                     <input className='inputFields' value={this.state.studentName} onChange={this.handlehanges} placeholder='הכנס את שם התלמיד...' name='studentName'></input>
 
                     <label for='userName'>שם משתמש:</label>
+                    <p class='error' style={{ display: this.state.userNameError.toShow }}>{this.state.userNameError.mess}</p>
                     <input className='inputFields' value={this.state.userName} onChange={this.handlehanges} placeholder='הכנס שם משתמש' name='userName'></input>
 
                     <label for='password'>סיסמא:</label>
+                    <p class='error' style={{ display: this.state.passwordError.toShow }}>{this.state.passwordError.mess}</p>
                     <input className='inputFields' value={this.state.password} onChange={this.handlehanges} type='password' placeholder='הכנס סיסמא' name='password'></input>
 
-                    <label>בית ספר:</label>
-                    <Select placeholder='בחר...' 
-                            styles={SelectStyle()} 
-                            options={this.makeSchoolOption()} 
-                            onChange={this.chooseSchool}
-                            placeholder='שייך לבית ספר'/>
+                    <label className='addStudentLabel'>בית ספר:</label>
+                    <p class='error' style={{ display: this.state.schoolNameError.toShow }}>{this.state.schoolNameError.mess}</p>
+                    <Select className='schoolSelection'
+                        id='addStudentSchoolSelection'
+                        placeholder='בחר...'
+                        styles={SelectStyle()}
+                        options={this.makeSchoolOption()}
+                        onChange={this.chooseSchool}
+                        placeholder='שייך לבית ספר' />
 
-                    <label>כיתה:</label>
+                    <label className='addStudentLabel'>כיתה:</label>
                     {
                         this.returnClassesSelections()
                     }
 
-                    <div className='addSomethingNew' onClick={this.addClassOption}>
+                    <div className='addSomethingNew addClassToStudent' onClick={this.addClassOption}>
                         <img className='addIcon' src='/icons/addicon.svg'></img>
                         <p className='addTitle'>הוסף כיתה</p>
                     </div>
                 </form>
-                <button className="saveButton">שמור</button>
+                <button className="saveButton" onClick={this.saveButton}>שמור</button>
             </div>
-         );
+        );
     }
 }
- 
+
 export default AddStudent;
