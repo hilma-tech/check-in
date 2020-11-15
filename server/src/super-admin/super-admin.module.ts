4470f7823b4f703772ddce9074b3ddd63b147a9f
BatZionR <batzion@carmel6000.amitnet.org>
2020-11-15 16:54:35 +0200

    partial superadmin user, partial field

---
+++ server/src/super-admin/super-admin.module.ts
@@ -0,0 +1,11 @@
+import { UserModule } from '@hilma/auth-nest';
+import { Module } from '@nestjs/common';
+import { SuperAdminController } from './super-admin.controller';
+import { SuperAdminService } from './super-admin.service';
+
+@Module({
+  imports: [UserModule],
+  controllers: [SuperAdminController],
+  providers: [SuperAdminService]
+})
+export class SuperAdminModule {}
