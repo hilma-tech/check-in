aff395ed174e5d2eea643a22f93175137fb3ba5a
pedutShaspi <pedutshaspi@carmel6000.amitnet.org>
2020-11-22 11:36:49 +0200

    comment out the add button

--- client/src/component/superAdmin/GeneralTable.jsx
+++ client/src/component/superAdmin/GeneralTable.jsx
@@ -39,7 +39,7 @@ class GeneralTable extends React.Component {
             })
           }
         </div>
-        <div className="addingButton" onClick={this.onClickAdd}></div>
+        {/* <div className="addingButton" onClick={this.onClickAdd}></div> */}
       </div>
     );
   }
