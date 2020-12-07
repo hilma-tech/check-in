28a8618f397276178d2a2cd0fe1fa0e224e38e8c
Ubuntu <CarmelBot@Carmelbot.f2mwmkabv5hezpugmvswrdqloh.ax.internal.cloudapp.net>
2020-12-07 13:01:22 +0000

    fixes related to deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(8080);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
