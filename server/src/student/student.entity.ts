0d1e71dc278bfabcf7dffca3ccfa2dd6a9fecdb8
pedutShaspi <pedutshaspi@carmel6000.amitnet.org>
2020-12-14 13:30:23 +0200

    add student, school, teacher entitys

---
+++ server/src/student/student.entity.ts
@@ -0,0 +1,31 @@
+import { Field } from "src/field/field.entity";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  MinKey
+} from "typeorm";
+import {
+  IsDefined,
+  IsString,
+  Length,
+  Matches,
+  IsBoolean
+} from "class-validator";
+import { User } from '@hilma/auth-nest';
+
+@Entity()
+export class Student extends User{
+  @IsDefined()
+  @IsString()
+  @Length(1, 30)
+  @Column({ type: "varchar", length: 50})
+  name: string;
+
+  @IsDefined()
+  @IsString()
+  @Length(1, 30)
+  @Column({ type: "varchar", length: 50})
+  email: string;
+}
---
+++ server/src/teacher/teacher.entity.ts
@@ -0,0 +1,51 @@
+import { Field } from "src/field/field.entity";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  MinKey,
+  Timestamp
+} from "typeorm";
+import {
+  IsDefined,
+  IsString,
+  Length,
+  Matches,
+  IsBoolean
+} from "class-validator";
+import { User } from '@hilma/auth-nest';
+import { School } from "src/school/school.entity";
+
+@Entity()
+export class Teacher extends User{
+  @IsDefined()
+  @IsString()
+  @Length(1, 30)
+  @Column({ type: "varchar", length: 50})
+  firstName: string;
+
+  @IsDefined()
+  @IsString()
+  @Length(1, 30)
+  @Column({ type: "varchar", length: 50})
+  lastName: string;
+
+  @IsDefined()
+  @IsString()
+  @Length(1, 30)
+  @Column({ type: "varchar", length: 50})
+  email: string;
+
+  @OneToMany(
+    type => School,
+    school => school.id
+  )
+  schoolId: School;
+
+//   @Column()
+//   created: Timestamp;
+
+//   @Column()
+//   updated: Timestamp;
+}
