8a3f0e03e388b4c69be7999d991599e9782b955d
shira6657 <shiragold@carmel6000.amitnet.org>
2020-12-21 10:54:06 +0200

    allow fetch from Daniel's domain

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,17 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "https://danielsoloway.com",
+    methods: "GET",
+    allowedHeaders: "Content-Type, Accept"
+
+  });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
 bootstrap();
+
+
