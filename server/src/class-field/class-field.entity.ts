4d7bf5a44d39c62d40419d0b699de9308070a7c3
BatZionR <batzion@carmel6000.amitnet.org>
2021-01-28 13:04:44 +0200

    created class-field entity

--- server/src/class-field/class-field.entity.ts
+++ server/src/class-field/class-field.entity.ts
@@ -1,8 +1,11 @@
+import { type } from 'os';
+import { Classroom } from 'src/classroom/classroom.entity';
+import { Field } from 'src/field/field.entity';
 import {
   Column,
   Entity,
-  JoinTable,
-  ManyToMany,
+  JoinColumn,
+  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -11,4 +14,14 @@ export class ClassField {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
+  new_value: string;
+
+  @OneToOne(type => Classroom) 
+  @JoinColumn({name: "class_id"}) 
+   class_id: Classroom;
+
+  @OneToOne(type => Field)
+  @JoinColumn({name: "field_id"})
+  field_id: Field;
 }
